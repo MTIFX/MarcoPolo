e80c69c283f741e4cd0a1059055473f4a32b0372
MTIFX <pkwiatkowski@markettraders.com>
2017-08-02 17:26:15 -0400

    Update and rename candlebuilder.py to candle.py

---
+++ app/candle.py
@@ -0,0 +1,26 @@
+# candlebuilder
+
+
+class candle():
+    
+    
+    
+
+
+    def Timeframe(self, tf):
+        pass
+
+
+
+    def OHLC(self, ohlc):
+        pass
+
+
+
+    def DateTime(self):
+        pass
+
+
+
+    def Index(self):
+        pass 
