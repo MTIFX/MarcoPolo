65ebf843aeb92e29730b4ae1adcd8a68d3bb4f14
MTIFX <pkwiatkowski@markettraders.com>
2017-08-02 17:45:49 -0400

    Update and rename distributed.py to cloud.py

---
+++ app/cloud.py
@@ -0,0 +1,25 @@
+# cloud -> distributed 
+
+
+class cloud():
+        
+    
+    def State(self):
+        pass
+    
+    
+    
+    def Write(self):
+        pass
+    
+    
+    
+    def Read(self):
+        pass
+    
+
+    
+    
+    
+    
+    
