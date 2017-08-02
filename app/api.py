7b8117c8b786f56a14ac5ff9bdda15cd2b0d00ba
MTIFX <pkwiatkowski@markettraders.com>
2017-08-02 17:24:43 -0400

    Update api.py

--- app/api.py
+++ app/api.py
@@ -1,1 +1,47 @@
+# api
+
+'''
+Features:
+
+tick
+OHLC
+
+M1, M5, M10, M15, M30, H1, H2, H3, H4, H8, D1, W1, MTH1
+
+
+Bar builder elements: 
+
+Timeframe
+OHLC
+DateTime
+Index
+
+
+'''
+
+class api():
+
+
+
+    def Timeframe(self, tf):
+        pass
+
+
+
+    def OHLC(self, ohlc):
+        pass
+
+
+
+    def DateTime(self):
+        pass
+
+
+
+    def Index(self):
+        pass 
+
+
+
+
 
