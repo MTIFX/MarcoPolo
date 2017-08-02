854319b918b71ae94e3ed54e53dab58e4c4f60e9
MTIFX <pkwiatkowski@markettraders.com>
2017-08-02 17:52:20 -0400

    Update state.py

--- app/state.py
+++ app/state.py
@@ -2,3 +2,31 @@
 
 
 class state():
+    
+    
+    
+    def Feed(self, node):
+        pass
+    
+    
+    
+    def Cloud(self, node):
+        pass
+    
+    
+    
+    def General(self):
+        pass
+    
+    
+    
+    def Node(self, node):
+        pass
+    
+    
+    
+    
+
+    
+    
+
