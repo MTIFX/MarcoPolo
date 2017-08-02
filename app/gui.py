061a250bd084d8475aa13df0df69050108eabe42
MTIFX <pkwiatkowski@markettraders.com>
2017-08-02 18:41:51 -0400

    Update gui.py

--- app/gui.py
+++ app/gui.py
@@ -27,3 +27,20 @@ class gui():
     
     
     
+    def CheckForSharedPairs(self, nodeOne, nodeTwo):
+        pass # warn the user if we only have 1 data feed, meaning if that one data source goes down, feed will go down
+    
+    
+    
+    def InformNewPairAdded(self):
+        pass # inform the user a new pair has been added to the feed, give option to add new pair
+    
+    
+    
+    def AddNewPair(self):
+        pass
+
+
+    
+    
+    
