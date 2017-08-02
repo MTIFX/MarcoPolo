e3dad075bfd748445f8f569823668f43a26b28a7
MTIFX <pkwiatkowski@markettraders.com>
2017-08-02 17:43:23 -0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,25 @@
+# database
 
+
+class database():
+    
+    
+    
+    def Insert(self, x, y):
+        pass
+    
+    
+    
+    def Remove(self, x, y):
+        pass
+    
+    
+    
+    def Read(self, x, y):
+        pass
+    
+    
+    
+    
+    
+    
