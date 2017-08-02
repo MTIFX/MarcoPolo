dc2021eaf6763e26b5f0372ee6cc2c95eb259f2a
MTIFX <pkwiatkowski@markettraders.com>
2017-08-02 17:39:01 -0400

    Update node.py

--- app/node.py
+++ app/node.py
@@ -1,1 +1,41 @@
 
+
+class node():
+    
+    
+    
+    def Broker(self, name):
+        pass
+    
+    
+    
+    def Tick(self):
+        pass
+    
+    
+    
+    def Up(self):
+        # node is up and sending data 
+        pass
+    
+    
+    
+    def Down(self):
+        # node is down and something is wrong
+        pass
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
