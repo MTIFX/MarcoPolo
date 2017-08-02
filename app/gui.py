1cd7a8cdbfad018fcb92e4e033d35d5013b70bf3
MTIFX <pkwiatkowski@markettraders.com>
2017-08-02 18:26:08 -0400

    Update gui.py

--- app/gui.py
+++ app/gui.py
@@ -1,1 +1,29 @@
+# gui
 
+
+
+class gui():
+    
+    
+    
+    def FeedStatus(self, node):
+        pass
+    
+    
+    
+    def SelectFeed(self, node, coin):
+        pass
+    
+    
+    
+    def AddCoinFeed(self, node, coin):
+        pass
+    
+    Contact GitHub API Training Shop Blog About
+
+    
+    def RemoveCoinFeed(self, node, coin):
+        pass
+    
+    
+    
