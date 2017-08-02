e490cd7652a2fd9a22ee989a053defdfa5e75cda
MTIFX <pkwiatkowski@markettraders.com>
2017-08-02 18:31:30 -0400

    Update feed.py

--- app/feed.py
+++ app/feed.py
@@ -1,1 +1,28 @@
+# feed
 
+import node
+node = node.node()
+
+
+
+class feed():
+    
+    
+    
+    def TickTime(self, node, pair):
+        return time
+    
+    
+    
+    def TickValue(self, node, pair, time):
+        return value
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
